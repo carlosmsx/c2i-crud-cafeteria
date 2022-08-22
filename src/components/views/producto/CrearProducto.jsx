448c57a5d6f50def2d75e09085041abe521a82bf
carlosmsx <carlosmsx@gmail.com>
2022-08-22 18:29:31 -0300

    maquetado del form

--- src/components/views/producto/CrearProducto.jsx
+++ src/components/views/producto/CrearProducto.jsx
@@ -7,9 +7,25 @@ const CrearProducto = () => {
             <h1 className="display-4 mt-5">Crear Producto</h1>
             <hr />
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                <Form.Group className="mb-3" controlId="formNombreProducto">
+                    <Form.Label>Nombre Producto</Form.Label>
+                    <Form.Control type="text" placeholder="Ej: Te" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPrecio">
+                    <Form.Label>Precio</Form.Label>
+                    <Form.Control type="text" placeholder="100" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formImagen">
+                    <Form.Label>Imagen</Form.Label>
+                    <Form.Control type="text" placeholder="Ej> https://..." />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formCategoria">
+                    <Form.Label>Categoria</Form.Label>
+                    <Form.Select>
+                        <option value="">Seleccione una opción</option>
+                        <option value="">Opción uno</option>
+                        <option value="">Opción dos</option>
+                    </Form.Select>
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
