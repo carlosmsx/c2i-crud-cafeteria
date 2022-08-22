b85f1c1eae8abd76d2a5fea7c9dcabb85326360e
carlosmsx <carlosmsx@gmail.com>
2022-08-22 20:04:30 -0300

    agregada variable de entorno, agregado state y useefect para hacer el fetch

--- src/components/views/producto/AdminProductos.jsx
+++ src/components/views/producto/AdminProductos.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import {Table,Button} from "react-bootstrap";
 import ItemProducto from './ItemProducto';
 
 const AdminProductos = () => {
+    const URL_API = process.env.REACT_APP_API_CAFETERIA;
+
+    const [productos, setProductos] = useState([]);
+ 
+    useEffect(()=>{
+        consultarAPI();
+    },[])
+
+    const consultarAPI = async () =>{
+        try{
+            //GET
+            const respuesta = await fetch(URL_API);
+            const listaProductos = await respuesta.json();
+            setProductos(listaProductos);
+        }
+        catch(error)
+        {
+            console.log(error);
+            //mostrar mensaje: espere unos minutos y vuelva a intentarlo
+        }
+    }
+
     return (
         <section className="container">
             <div className="d-flex justify-content-between align-item-center mt-5">
@@ -23,9 +45,8 @@ const AdminProductos = () => {
                 </thead>
                 <tbody>
                     {
-
+                        productos.map((producto)=><ItemProducto key={producto.id} producto={producto}/>)
                     }
-                    <ItemProducto/>
                 </tbody>
             </Table>
         </section>
--- src/components/views/producto/ItemProducto.jsx
+++ src/components/views/producto/ItemProducto.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import {Button} from 'react-bootstrap';
 
-const ItemProducto = () => {
+const ItemProducto = ({producto}) => {
+    const {nombreProducto, id, precio, imagen, categoria}={...producto};
     return (
         <tr>
-            <td>1</td>
-            <td>Cafe</td>
-            <td>$700</td>
-            <td>
-                https://images.pexels.com/photos/4109850/pexels-photo-4109850.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-4109850.jpg&fm=jpg
-            </td>
-            <td>Bebida caliente</td>
+            <td>{id}</td>
+            <td>{nombreProducto}</td>
+            <td>{precio}</td>
+            <td>{imagen}</td>
+            <td>{categoria}</td>
             <td>
                 <Button variant="warning">Editar</Button>
                 <Button variant="danger">Borrar</Button>
